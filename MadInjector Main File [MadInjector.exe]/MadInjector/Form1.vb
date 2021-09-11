a515adfbdf4d8594456c3fb61726e60a09b90b07
ByMynix.xyz <81905688+ByMynix@users.noreply.github.com>
2021-09-11 22:18:48 +0200

    Add files via upload

---
+++ MadInjector Main File [MadInjector.exe]/MadInjector/Form1.vb
@@ -0,0 +1,115 @@
+﻿Imports System.IO
+Imports System.Management
+Imports System.Net
+Imports System.Threading
+Imports System.Diagnostics
+Imports System.Diagnostics.Process
+Public Class Form1
+    Private Sub MetroButton2_Click(sender As Object, e As EventArgs) Handles MetroButton2.Click
+        Try
+            OpenFileDialog1.ShowDialog()
+            My.Computer.FileSystem.CopyFile(OpenFileDialog1.FileName, "C:\MadInjector\ServiceHub.Microsoft.dll", overwrite:=True)
+            MetroButton1.Enabled = True
+        Catch ex As Exception
+            MessageBox.Show("No File selected")
+        End Try
+    End Sub
+
+    Private Sub MetroButton1_Click(sender As Object, e As EventArgs) Handles MetroButton1.Click
+        Try
+            Dim p = Process.GetProcessesByName("csgo")
+            If p.Count > 0 Then
+                Dim info As New ProcessStartInfo
+                info.FileName = ("C:\MadInjector\ServiceHub.Microsoft.exe")
+                info.Verb = "runas"
+                Process.Start(info)
+                My.Computer.Audio.Play("C:\MadInjector\ServiceHub.MicrosoftSound.wav", AudioPlayMode.Background)
+            Else
+                MessageBox.Show("Process not found! Start CSGO first before you try to inject")
+            End If
+        Catch ex As Exception
+            MessageBox.Show("File not found! Your antivirus may have removed the essential files. Check that your antivirus is deactivated and use the 'Repair all Files' method")
+        End Try
+    End Sub
+
+    Private Sub MetroLabel1_Click(sender As Object, e As EventArgs) Handles MetroLabel1.Click
+        MessageBox.Show("=====================
+
+--How to Use--
+
+- Turn off any antivirus on your computer
+- Start CSGO
+- Click on [Choose File] and choose your file (cheat.dll)
+- Click on [Inject]
+- Enjoy !
+
+--------------")
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Dim client As WebClient = New WebClient()
+        If MetroTextBox1.Text = client.DownloadString("https://madinjector.bymynix.xyz/Update%20Checker%201.6.txt") Then
+
+        Else
+            Timer1.Interval = 3 * 1000
+            Timer1.Start()
+        End If
+        Process.Start("https://bit.ly/cheatermad")
+        MetroButton1.Enabled = False
+    End Sub
+
+    Private Sub MetroLink2_Click(sender As Object, e As EventArgs)
+        Process.Start("https://bit.ly/cheatermad")
+    End Sub
+
+    Private Sub MetroButton3_Click(sender As Object, e As EventArgs) Handles MetroButton3.Click
+        Try
+            Dim info As New ProcessStartInfo
+            info.FileName = ("C:\MadInjector\Repair.exe")
+            info.Verb = "runas"
+            Process.Start(info)
+            Me.Close()
+        Catch ex As Exception
+            MessageBox.Show("File not found! Your antivirus may have removed the essential files. Check that your antivirus is deactivated and reinstall MadInjector")
+        End Try
+    End Sub
+
+    Private Sub MetroButton4_Click(sender As Object, e As EventArgs) Handles MetroButton4.Click
+        Dim url As String = "https://madinjector.bymynix.xyz/Changelogs.txt"
+        Try
+            Using wc As New System.Net.WebClient()
+                MsgBox(wc.DownloadString(url))
+            End Using
+        Catch ex As Exception
+            MsgBox(ex.Message)
+        End Try
+    End Sub
+
+    Private Sub Timer1_Tick(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Timer1.Tick
+        MessageBox.Show("New update available! The update will start automatically.")
+        Dim h As New Form2
+        h.Show()
+        Me.Close()
+
+    End Sub
+
+    Private Sub PictureBox2_Click(sender As Object, e As EventArgs) Handles PictureBox2.Click
+        Process.Start("http://discord.bymynix.xyz/")
+    End Sub
+
+    Private Sub PictureBox8_Click(sender As Object, e As EventArgs) Handles PictureBox8.Click
+        Process.Start("https://projects.bymynix.xyz/")
+    End Sub
+
+    Private Sub PictureBox3_Click(sender As Object, e As EventArgs) Handles PictureBox3.Click
+        Process.Start("https://corsair.wtf/")
+    End Sub
+
+    Private Sub PictureBox5_Click(sender As Object, e As EventArgs) Handles PictureBox5.Click
+        Process.Start("https://bit.ly/cheaterninja")
+    End Sub
+
+    Private Sub MetroLink2_Click_1(sender As Object, e As EventArgs) Handles MetroLink2.Click
+        Process.Start("https://bit.ly/cheatermad")
+    End Sub
+End Class
---
+++ MadInjector Main File [MadInjector.exe]/MadInjector/Form2.vb
@@ -0,0 +1,38 @@
+﻿Public Class Form2
+    Private Sub Form2_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Dim uri As System.Uri = New System.Uri("https://madinjector.bymynix.xyz/MadInjector%20Setup.exe")
+        Dim webclient As System.Net.WebClient = New System.Net.WebClient()
+
+        Dim path As String = New String(System.IO.Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "C:\MadInjector\Updater.exe"))
+        Dim fileInfo As System.IO.FileInfo = New System.IO.FileInfo(path)
+        If Not System.IO.Directory.Exists(fileInfo.Directory.FullName) Then
+            System.IO.Directory.CreateDirectory(fileInfo.Directory.FullName)
+        End If
+
+
+        AddHandler webclient.DownloadFileCompleted, AddressOf webclient_DownloadDataCompleted
+        AddHandler webclient.DownloadProgressChanged, AddressOf DownloadProgressChanged
+
+        webclient.DownloadFileAsync(uri, path)
+    End Sub
+
+    Private Sub DownloadProgressChanged(ByVal sender As Object, ByVal e As Net.DownloadProgressChangedEventArgs)
+        MetroProgressBar1.Value = e.ProgressPercentage
+    End Sub
+
+    Private Sub webclient_DownloadDataCompleted(ByVal sender As Object, ByVal e As System.ComponentModel.AsyncCompletedEventArgs)
+        MessageBox.Show("Update successfully downloaded. The setup starts automatically")
+        Try
+            Process.Start("C:\MadInjector\Updater.exe")
+        Catch ex As Exception
+            MessageBox.Show("File not found! Your antivirus may have removed the update files. Check that your antivirus is deactivated. The program will close automatically")
+            Me.Close()
+        End Try
+        Me.Close()
+
+
+
+
+
+    End Sub
+End Class
