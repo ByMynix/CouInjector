007ad24b04cd60af912306ae4ca6ef70b201199e
ByMynixDE <81905688+ByMynix@users.noreply.github.com>
2022-04-20 22:40:47 +0200

    Add files via upload

---
+++ CouInjector Main File [CouInjector.exe]/CouInjector/Form1.Designer.vb
@@ -0,0 +1,315 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits MetroFramework.Forms.MetroForm
+
+    'Das Formular überschreibt den Löschvorgang, um die Komponentenliste zu bereinigen.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Wird vom Windows Form-Designer benötigt.
+    Private components As System.ComponentModel.IContainer
+
+    'Hinweis: Die folgende Prozedur ist für den Windows Form-Designer erforderlich.
+    'Das Bearbeiten ist mit dem Windows Form-Designer möglich.  
+    'Das Bearbeiten mit dem Code-Editor ist nicht möglich.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.MetroTabControl1 = New MetroFramework.Controls.MetroTabControl()
+        Me.MetroTabPage1 = New MetroFramework.Controls.MetroTabPage()
+        Me.MetroButton1 = New MetroFramework.Controls.MetroButton()
+        Me.MetroButton2 = New MetroFramework.Controls.MetroButton()
+        Me.MetroTabPage2 = New MetroFramework.Controls.MetroTabPage()
+        Me.MetroLabel2 = New MetroFramework.Controls.MetroLabel()
+        Me.MetroToggle2 = New MetroFramework.Controls.MetroToggle()
+        Me.MetroButton4 = New MetroFramework.Controls.MetroButton()
+        Me.MetroButton3 = New MetroFramework.Controls.MetroButton()
+        Me.OpenFileDialog1 = New System.Windows.Forms.OpenFileDialog()
+        Me.MetroLabel1 = New MetroFramework.Controls.MetroLabel()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox2 = New System.Windows.Forms.PictureBox()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.PictureBox8 = New System.Windows.Forms.PictureBox()
+        Me.MetroLabel5 = New MetroFramework.Controls.MetroLabel()
+        Me.MetroLabel4 = New MetroFramework.Controls.MetroLabel()
+        Me.PictureBox4 = New System.Windows.Forms.PictureBox()
+        Me.MetroTabControl1.SuspendLayout()
+        Me.MetroTabPage1.SuspendLayout()
+        Me.MetroTabPage2.SuspendLayout()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox8, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox4, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'MetroTabControl1
+        '
+        Me.MetroTabControl1.Controls.Add(Me.MetroTabPage1)
+        Me.MetroTabControl1.Controls.Add(Me.MetroTabPage2)
+        Me.MetroTabControl1.Location = New System.Drawing.Point(23, 63)
+        Me.MetroTabControl1.Name = "MetroTabControl1"
+        Me.MetroTabControl1.SelectedIndex = 0
+        Me.MetroTabControl1.Size = New System.Drawing.Size(366, 156)
+        Me.MetroTabControl1.TabIndex = 0
+        Me.MetroTabControl1.UseSelectable = True
+        '
+        'MetroTabPage1
+        '
+        Me.MetroTabPage1.Controls.Add(Me.MetroButton1)
+        Me.MetroTabPage1.Controls.Add(Me.MetroButton2)
+        Me.MetroTabPage1.HorizontalScrollbarBarColor = True
+        Me.MetroTabPage1.HorizontalScrollbarHighlightOnWheel = False
+        Me.MetroTabPage1.HorizontalScrollbarSize = 10
+        Me.MetroTabPage1.Location = New System.Drawing.Point(4, 38)
+        Me.MetroTabPage1.Name = "MetroTabPage1"
+        Me.MetroTabPage1.Size = New System.Drawing.Size(358, 114)
+        Me.MetroTabPage1.TabIndex = 0
+        Me.MetroTabPage1.Text = "CouInjector  I"
+        Me.MetroTabPage1.VerticalScrollbarBarColor = True
+        Me.MetroTabPage1.VerticalScrollbarHighlightOnWheel = False
+        Me.MetroTabPage1.VerticalScrollbarSize = 10
+        '
+        'MetroButton1
+        '
+        Me.MetroButton1.BackColor = System.Drawing.SystemColors.Control
+        Me.MetroButton1.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.MetroButton1.DisplayFocus = True
+        Me.MetroButton1.Location = New System.Drawing.Point(3, 62)
+        Me.MetroButton1.Name = "MetroButton1"
+        Me.MetroButton1.Size = New System.Drawing.Size(352, 49)
+        Me.MetroButton1.Style = MetroFramework.MetroColorStyle.Red
+        Me.MetroButton1.TabIndex = 7
+        Me.MetroButton1.Text = "Inject"
+        Me.MetroButton1.Theme = MetroFramework.MetroThemeStyle.Dark
+        Me.MetroButton1.UseSelectable = True
+        '
+        'MetroButton2
+        '
+        Me.MetroButton2.BackColor = System.Drawing.SystemColors.Control
+        Me.MetroButton2.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.MetroButton2.DisplayFocus = True
+        Me.MetroButton2.Location = New System.Drawing.Point(3, 7)
+        Me.MetroButton2.Name = "MetroButton2"
+        Me.MetroButton2.Size = New System.Drawing.Size(352, 49)
+        Me.MetroButton2.Style = MetroFramework.MetroColorStyle.Red
+        Me.MetroButton2.TabIndex = 6
+        Me.MetroButton2.Text = "Choose File"
+        Me.MetroButton2.Theme = MetroFramework.MetroThemeStyle.Dark
+        Me.MetroButton2.UseSelectable = True
+        '
+        'MetroTabPage2
+        '
+        Me.MetroTabPage2.Controls.Add(Me.MetroLabel2)
+        Me.MetroTabPage2.Controls.Add(Me.MetroToggle2)
+        Me.MetroTabPage2.Controls.Add(Me.MetroButton4)
+        Me.MetroTabPage2.Controls.Add(Me.MetroButton3)
+        Me.MetroTabPage2.HorizontalScrollbarBarColor = True
+        Me.MetroTabPage2.HorizontalScrollbarHighlightOnWheel = False
+        Me.MetroTabPage2.HorizontalScrollbarSize = 10
+        Me.MetroTabPage2.Location = New System.Drawing.Point(4, 38)
+        Me.MetroTabPage2.Name = "MetroTabPage2"
+        Me.MetroTabPage2.Size = New System.Drawing.Size(358, 114)
+        Me.MetroTabPage2.TabIndex = 1
+        Me.MetroTabPage2.Text = "Settings  I"
+        Me.MetroTabPage2.VerticalScrollbarBarColor = True
+        Me.MetroTabPage2.VerticalScrollbarHighlightOnWheel = False
+        Me.MetroTabPage2.VerticalScrollbarSize = 10
+        '
+        'MetroLabel2
+        '
+        Me.MetroLabel2.AutoSize = True
+        Me.MetroLabel2.BackColor = System.Drawing.Color.Transparent
+        Me.MetroLabel2.Cursor = System.Windows.Forms.Cursors.Default
+        Me.MetroLabel2.Location = New System.Drawing.Point(158, 1)
+        Me.MetroLabel2.Name = "MetroLabel2"
+        Me.MetroLabel2.Size = New System.Drawing.Size(141, 19)
+        Me.MetroLabel2.TabIndex = 54
+        Me.MetroLabel2.Text = "Webpages after Start :"
+        '
+        'MetroToggle2
+        '
+        Me.MetroToggle2.AutoSize = True
+        Me.MetroToggle2.Checked = True
+        Me.MetroToggle2.CheckState = System.Windows.Forms.CheckState.Checked
+        Me.MetroToggle2.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.MetroToggle2.DisplayStatus = False
+        Me.MetroToggle2.Location = New System.Drawing.Point(305, 3)
+        Me.MetroToggle2.Name = "MetroToggle2"
+        Me.MetroToggle2.Size = New System.Drawing.Size(50, 17)
+        Me.MetroToggle2.TabIndex = 53
+        Me.MetroToggle2.Text = "An"
+        Me.MetroToggle2.UseSelectable = True
+        '
+        'MetroButton4
+        '
+        Me.MetroButton4.BackColor = System.Drawing.SystemColors.Control
+        Me.MetroButton4.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.MetroButton4.DisplayFocus = True
+        Me.MetroButton4.Location = New System.Drawing.Point(3, 72)
+        Me.MetroButton4.Name = "MetroButton4"
+        Me.MetroButton4.Size = New System.Drawing.Size(352, 39)
+        Me.MetroButton4.Style = MetroFramework.MetroColorStyle.Red
+        Me.MetroButton4.TabIndex = 8
+        Me.MetroButton4.Text = "Changelogs"
+        Me.MetroButton4.Theme = MetroFramework.MetroThemeStyle.Dark
+        Me.MetroButton4.UseSelectable = True
+        '
+        'MetroButton3
+        '
+        Me.MetroButton3.BackColor = System.Drawing.SystemColors.Control
+        Me.MetroButton3.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.MetroButton3.DisplayFocus = True
+        Me.MetroButton3.Location = New System.Drawing.Point(3, 26)
+        Me.MetroButton3.Name = "MetroButton3"
+        Me.MetroButton3.Size = New System.Drawing.Size(352, 40)
+        Me.MetroButton3.Style = MetroFramework.MetroColorStyle.Red
+        Me.MetroButton3.TabIndex = 7
+        Me.MetroButton3.Text = "Repair all Files"
+        Me.MetroButton3.Theme = MetroFramework.MetroThemeStyle.Dark
+        Me.MetroButton3.UseSelectable = True
+        '
+        'OpenFileDialog1
+        '
+        Me.OpenFileDialog1.FileName = "cheat.dll"
+        '
+        'MetroLabel1
+        '
+        Me.MetroLabel1.AutoSize = True
+        Me.MetroLabel1.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.MetroLabel1.Location = New System.Drawing.Point(305, 41)
+        Me.MetroLabel1.Name = "MetroLabel1"
+        Me.MetroLabel1.Size = New System.Drawing.Size(84, 19)
+        Me.MetroLabel1.TabIndex = 2
+        Me.MetroLabel1.Text = "[How to Use]"
+        Me.MetroLabel1.Theme = MetroFramework.MetroThemeStyle.Dark
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.BackgroundImage = CType(resources.GetObject("PictureBox1.BackgroundImage"), System.Drawing.Image)
+        Me.PictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.PictureBox1.Location = New System.Drawing.Point(153, 14)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(51, 46)
+        Me.PictureBox1.TabIndex = 54
+        Me.PictureBox1.TabStop = False
+        '
+        'PictureBox2
+        '
+        Me.PictureBox2.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.PictureBox2.Image = CType(resources.GetObject("PictureBox2.Image"), System.Drawing.Image)
+        Me.PictureBox2.Location = New System.Drawing.Point(367, 221)
+        Me.PictureBox2.Name = "PictureBox2"
+        Me.PictureBox2.Size = New System.Drawing.Size(22, 19)
+        Me.PictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox2.TabIndex = 56
+        Me.PictureBox2.TabStop = False
+        '
+        'Timer1
+        '
+        '
+        'PictureBox8
+        '
+        Me.PictureBox8.BackgroundImage = CType(resources.GetObject("PictureBox8.BackgroundImage"), System.Drawing.Image)
+        Me.PictureBox8.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.PictureBox8.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.PictureBox8.Location = New System.Drawing.Point(342, 221)
+        Me.PictureBox8.Name = "PictureBox8"
+        Me.PictureBox8.Size = New System.Drawing.Size(19, 19)
+        Me.PictureBox8.TabIndex = 72
+        Me.PictureBox8.TabStop = False
+        '
+        'MetroLabel5
+        '
+        Me.MetroLabel5.AutoSize = True
+        Me.MetroLabel5.Cursor = System.Windows.Forms.Cursors.Default
+        Me.MetroLabel5.Location = New System.Drawing.Point(302, 221)
+        Me.MetroLabel5.Name = "MetroLabel5"
+        Me.MetroLabel5.Size = New System.Drawing.Size(34, 19)
+        Me.MetroLabel5.TabIndex = 79
+        Me.MetroLabel5.Text = "Dev:"
+        Me.MetroLabel5.Theme = MetroFramework.MetroThemeStyle.Dark
+        '
+        'MetroLabel4
+        '
+        Me.MetroLabel4.AutoSize = True
+        Me.MetroLabel4.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.MetroLabel4.Location = New System.Drawing.Point(23, 221)
+        Me.MetroLabel4.Name = "MetroLabel4"
+        Me.MetroLabel4.Size = New System.Drawing.Size(60, 19)
+        Me.MetroLabel4.TabIndex = 82
+        Me.MetroLabel4.Text = "Partners:"
+        Me.MetroLabel4.Theme = MetroFramework.MetroThemeStyle.Dark
+        '
+        'PictureBox4
+        '
+        Me.PictureBox4.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.PictureBox4.Image = CType(resources.GetObject("PictureBox4.Image"), System.Drawing.Image)
+        Me.PictureBox4.Location = New System.Drawing.Point(85, 221)
+        Me.PictureBox4.Name = "PictureBox4"
+        Me.PictureBox4.Size = New System.Drawing.Size(22, 19)
+        Me.PictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox4.TabIndex = 84
+        Me.PictureBox4.TabStop = False
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(412, 242)
+        Me.Controls.Add(Me.PictureBox4)
+        Me.Controls.Add(Me.MetroLabel4)
+        Me.Controls.Add(Me.MetroLabel5)
+        Me.Controls.Add(Me.PictureBox8)
+        Me.Controls.Add(Me.PictureBox2)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Controls.Add(Me.MetroLabel1)
+        Me.Controls.Add(Me.MetroTabControl1)
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.MaximizeBox = False
+        Me.Name = "Form1"
+        Me.Resizable = False
+        Me.Style = MetroFramework.MetroColorStyle.Yellow
+        Me.Text = "CouInjector"
+        Me.Theme = MetroFramework.MetroThemeStyle.Dark
+        Me.TransparencyKey = System.Drawing.Color.Snow
+        Me.MetroTabControl1.ResumeLayout(False)
+        Me.MetroTabPage1.ResumeLayout(False)
+        Me.MetroTabPage2.ResumeLayout(False)
+        Me.MetroTabPage2.PerformLayout()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox8, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox4, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents MetroTabControl1 As MetroFramework.Controls.MetroTabControl
+    Friend WithEvents MetroTabPage1 As MetroFramework.Controls.MetroTabPage
+    Friend WithEvents MetroButton1 As MetroFramework.Controls.MetroButton
+    Friend WithEvents MetroButton2 As MetroFramework.Controls.MetroButton
+    Friend WithEvents OpenFileDialog1 As OpenFileDialog
+    Friend WithEvents MetroLabel1 As MetroFramework.Controls.MetroLabel
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents PictureBox2 As PictureBox
+    Friend WithEvents MetroTabPage2 As MetroFramework.Controls.MetroTabPage
+    Friend WithEvents MetroButton4 As MetroFramework.Controls.MetroButton
+    Friend WithEvents MetroButton3 As MetroFramework.Controls.MetroButton
+    Friend WithEvents Timer1 As Timer
+    Friend WithEvents PictureBox8 As PictureBox
+    Friend WithEvents MetroLabel5 As MetroFramework.Controls.MetroLabel
+    Friend WithEvents MetroLabel2 As MetroFramework.Controls.MetroLabel
+    Friend WithEvents MetroToggle2 As MetroFramework.Controls.MetroToggle
+    Friend WithEvents MetroLabel4 As MetroFramework.Controls.MetroLabel
+    Friend WithEvents PictureBox4 As PictureBox
+End Class
---
+++ CouInjector Main File [CouInjector.exe]/CouInjector/Form1.vb
@@ -0,0 +1,119 @@
+﻿Imports System.IO
+Imports System.Management
+Imports System.Net
+Imports System.Threading
+Imports System.Diagnostics
+Imports System.Diagnostics.Process
+Public Class Form1
+    Private Sub MetroButton2_Click(sender As Object, e As EventArgs) Handles MetroButton2.Click
+        Try
+            OpenFileDialog1.ShowDialog()
+            My.Computer.FileSystem.CopyFile(OpenFileDialog1.FileName, "C:\CouInjector\ServiceHub.Microsoft.dll", overwrite:=True)
+            MetroButton1.Enabled = True
+        Catch ex As Exception
+            MessageBox.Show("No File selected")
+        End Try
+    End Sub
+
+    Private Sub MetroButton1_Click(sender As Object, e As EventArgs) Handles MetroButton1.Click
+        Try
+            Dim p = Process.GetProcessesByName("csgo")
+            If p.Count > 0 Then
+                Dim info As New ProcessStartInfo
+                info.FileName = ("C:\CouInjector\ServiceHub.Microsoft.exe")
+                info.Verb = "runas"
+                Process.Start(info)
+                My.Computer.Audio.Play("C:\CouInjector\ServiceHub.MicrosoftSound.wav", AudioPlayMode.Background)
+            Else
+                MessageBox.Show("Process not found! Start CSGO first before you try to inject")
+            End If
+        Catch ex As Exception
+            MessageBox.Show("File not found! Your antivirus may have removed the essential files. Check that your antivirus is deactivated and use the 'Repair all Files' method")
+        End Try
+    End Sub
+
+    Private Sub MetroLabel1_Click(sender As Object, e As EventArgs) Handles MetroLabel1.Click
+        MessageBox.Show("=====================
+
+--How to Use--
+
+- Turn off any antivirus on your computer
+- Start CSGO
+- Click on [Choose File] and choose your file (cheat.dll)
+- Click on [Inject]
+- Enjoy !
+
+--------------")
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        If File.Exists("C:\CouInjector\Settings.txt") Then
+            MetroToggle2.Checked = False
+        Else
+            Process.Start("http://dsc.gg/bymynixde")
+            Process.Start("https://bit.ly/bymynix--developments")
+            Process.Start("https://bit.ly/github-couinjector-bymynix")
+        End If
+        Dim client As WebClient = New WebClient()
+        If "No Updates available!" = client.DownloadString("https://bymynix.de/couinjector/Update%20Checker%201.9.txt") Then
+
+        Else
+            Timer1.Interval = 3 * 1000
+            Timer1.Start()
+        End If
+        MetroButton1.Enabled = False
+    End Sub
+
+    Private Sub MetroButton3_Click(sender As Object, e As EventArgs) Handles MetroButton3.Click
+        Try
+            Dim info As New ProcessStartInfo
+            info.FileName = ("C:\CouInjector\Repair.exe")
+            info.Verb = "runas"
+            Process.Start(info)
+            Me.Close()
+        Catch ex As Exception
+            MessageBox.Show("File not found! Your antivirus may have removed the essential files. Check that your antivirus is deactivated and reinstall CouInjector")
+        End Try
+    End Sub
+
+    Private Sub MetroButton4_Click(sender As Object, e As EventArgs) Handles MetroButton4.Click
+        Dim url As String = "https://bymynix.de/couinjector/Changelogs.txt"
+        Try
+            Using wc As New System.Net.WebClient()
+                MsgBox(wc.DownloadString(url))
+            End Using
+        Catch ex As Exception
+            MsgBox(ex.Message)
+        End Try
+    End Sub
+
+    Private Sub Timer1_Tick(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Timer1.Tick
+        Timer1.Stop()
+        MessageBox.Show("New update available! The update will start automatically.")
+        Dim h As New Form2
+        h.Show()
+        Me.Close()
+
+    End Sub
+
+    Private Sub PictureBox2_Click(sender As Object, e As EventArgs) Handles PictureBox2.Click
+        Process.Start("http://dsc.gg/bymynixde")
+    End Sub
+
+    Private Sub PictureBox8_Click(sender As Object, e As EventArgs) Handles PictureBox8.Click
+        Process.Start("https://bit.ly/bymynix--developments")
+    End Sub
+
+    Private Sub MetroToggle2_CheckedChanged(sender As Object, e As EventArgs) Handles MetroToggle2.Click
+        If MetroToggle2.Checked = False Then
+            System.IO.File.Create("C:\CouInjector\Settings.txt").Close()
+        End If
+        If MetroToggle2.Checked = True And File.Exists("C:\CouInjector\Settings.txt") Then
+            System.IO.File.Delete("C:\CouInjector\Settings.txt")
+        End If
+    End Sub
+
+    Private Sub PictureBox4_Click(sender As Object, e As EventArgs) Handles PictureBox4.Click
+        Process.Start("https://cheater.fun/")
+    End Sub
+End Class
