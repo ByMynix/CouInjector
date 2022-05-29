19e84af142da1e665c2231424b8b8fbea6388d91
ByMynix <j.r.riemer475@gmail.com>
2022-05-29 22:54:21 +0200

    Projektdateien hinzufügen.

---
+++ CouInjector Auto-Update/Module1.vb
@@ -0,0 +1,36 @@
+﻿Imports System.IO
+Imports System.Threading
+Module Module1
+    Sub Main()
+        Console.WriteLine("Wait for exit...(CouInjector)")
+        Dim T As New Process
+        With T
+            For Each p1 As Process In Process.GetProcessesByName("CouInjector")
+                p1.WaitForExit()
+            Next
+        End With
+
+        Console.WriteLine("Deleting old version...")
+        File.Delete(Environment.CurrentDirectory & "\CouInjector.exe")
+        Console.WriteLine("Downloading new version...")
+        Dim uri As System.Uri = New System.Uri("https://bymynix.de/couinjector/CouInjector.exe")
+        Dim webclient As System.Net.WebClient = New System.Net.WebClient()
+
+        Dim path As String = New String(System.Environment.CurrentDirectory & "\CouInjector.exe")
+        Dim fileInfo As System.IO.FileInfo = New System.IO.FileInfo(path)
+
+
+        AddHandler webclient.DownloadFileCompleted, AddressOf webclient_DownloadDataCompleted
+
+        webclient.DownloadFileAsync(uri, path)
+    End Sub
+
+    Private Sub webclient_DownloadDataCompleted(ByVal sender As Object, ByVal e As System.ComponentModel.AsyncCompletedEventArgs)
+        MsgBox("Update successfully downloaded. CouInjector starts automatically")
+
+        Process.Start(System.Environment.CurrentDirectory & "\CouInjector.exe")
+
+
+
+    End Sub
+End Module
---
+++ CouInjector Auto-Update/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' Allgemeine Informationen über eine Assembly werden über die folgenden
+' Attribute gesteuert. Ändern Sie diese Attributwerte, um die Informationen zu ändern,
+' die einer Assembly zugeordnet sind.
+
+' Werte der Assemblyattribute überprüfen
+
+<Assembly: AssemblyTitle("Updater")>
+<Assembly: AssemblyDescription("CouInjector Auto-Updater")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("CouInjector Auto-Updater")>
+<Assembly: AssemblyCopyright("Copyright ©  2022")>
+<Assembly: AssemblyTrademark("ByMynixDE")>
+
+<Assembly: ComVisible(False)>
+
+'Die folgende GUID wird für die typelib-ID verwendet, wenn dieses Projekt für COM verfügbar gemacht wird.
+<Assembly: Guid("328f53c1-8773-43c7-91ff-3830abffcb43")>
+
+' Versionsinformationen für eine Assembly bestehen aus den folgenden vier Werten:
+'
+'      Hauptversion
+'      Nebenversion
+'      Buildnummer
+'      Revision
+'
+' Sie können alle Werte angeben oder Standardwerte für die Build- und Revisionsnummern verwenden,
+' indem Sie "*" wie unten gezeigt eingeben:
+' <Assembly: AssemblyVersion("1.0.*")>
+
+<Assembly: AssemblyVersion("0.0.0.0")>
+<Assembly: AssemblyFileVersion("0.0.0.0")>
