08bb86bb424eafc9016bcc59b064ca69cfe3aaed
ByMynix <j.r.riemer475@gmail.com>
2022-05-31 21:19:59 +0200

    -DownloadFileCompleted Sub with DownloadFileAsync removed, otherwise it terminates the programme and doesn't switch to the other sub. Replaced with "DownloadFile" and the attached code -A sleep time of 3 seconds was introduced for each of the different console lines so that the user can easily follow the current status. -Changed some little stuff

--- CouInjector Auto-Update/Module1.vb
+++ CouInjector Auto-Update/Module1.vb
@@ -1,8 +1,10 @@
 ﻿Imports System.IO
 Imports System.Threading
 Module Module1
+    Dim AppPath = System.AppDomain.CurrentDomain.BaseDirectory
     Sub Main()
         Console.WriteLine("Wait for exit...(CouInjector)")
+        System.Threading.Thread.Sleep(3000)
         Dim T As New Process
         With T
             For Each p1 As Process In Process.GetProcessesByName("CouInjector")
@@ -11,26 +13,18 @@ Module Module1
         End With
 
         Console.WriteLine("Deleting old version...")
-        File.Delete(Environment.CurrentDirectory & "\CouInjector.exe")
+        System.Threading.Thread.Sleep(3000)
+        File.Delete(AppPath & "\CouInjector.exe")
         Console.WriteLine("Downloading new version...")
+        System.Threading.Thread.Sleep(3000)
         Dim uri As System.Uri = New System.Uri("https://bymynix.de/couinjector/CouInjector.exe")
         Dim webclient As System.Net.WebClient = New System.Net.WebClient()
-
-        Dim path As String = New String(System.Environment.CurrentDirectory & "\CouInjector.exe")
+        Dim path As String = New String(AppPath & "\CouInjector.exe")
         Dim fileInfo As System.IO.FileInfo = New System.IO.FileInfo(path)
 
+        webclient.DownloadFile(uri, path)
 
-        AddHandler webclient.DownloadFileCompleted, AddressOf webclient_DownloadDataCompleted
-
-        webclient.DownloadFileAsync(uri, path)
-    End Sub
-
-    Private Sub webclient_DownloadDataCompleted(ByVal sender As Object, ByVal e As System.ComponentModel.AsyncCompletedEventArgs)
         MsgBox("Update successfully downloaded. CouInjector starts automatically")
-
-        Process.Start(System.Environment.CurrentDirectory & "\CouInjector.exe")
-
-
-
+        Process.Start(AppPath & "\CouInjector.exe")
     End Sub
 End Module
